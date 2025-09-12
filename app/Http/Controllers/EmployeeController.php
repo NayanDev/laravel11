ee3ea9e8fda85d2bc2d93247ad31853f17e83ee6
Nayantaka <64335344+NayanDev@users.noreply.github.com>
2025-09-12 11:24:47 +0700

    update join masterdata

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -6,6 +6,7 @@ use App\Models\Department;
 use App\Models\Employee;
 use App\Models\Position;
 use App\Models\Qualification;
+use Idev\EasyAdmin\app\Helpers\Constant;
 use Idev\EasyAdmin\app\Http\Controllers\DefaultController;
 
 class EmployeeController extends DefaultController
@@ -29,9 +30,9 @@ class EmployeeController extends DefaultController
                     ['name' => 'No', 'column' => '#', 'order' => true], 
                     ['name' => 'Name', 'column' => 'first_name', 'order' => true], 
                     ['name' => 'Email', 'column' => 'email', 'order' => true], 
-                    ['name' => 'Department', 'column' => 'department_id', 'order' => true], 
-                    ['name' => 'Position', 'column' => 'position_id', 'order' => true], 
-                    ['name' => 'Qualification', 'column' => 'qualification_id', 'order' => true], 
+                    ['name' => 'Department', 'column' => 'department', 'order' => true], 
+                    ['name' => 'Position', 'column' => 'position', 'order' => true], 
+                    ['name' => 'Qualification', 'column' => 'qualification', 'order' => true], 
                     ['name' => 'Created at', 'column' => 'created_at', 'order' => true],
                     ['name' => 'Updated at', 'column' => 'updated_at', 'order' => true],
         ];
@@ -44,6 +45,92 @@ class EmployeeController extends DefaultController
         ];
     }
 
+    public function index()
+    {
+        $baseUrlExcel = route($this->generalUri.'.export-excel-default');
+        $baseUrlPdf = route($this->generalUri.'.export-pdf-default');
+
+        $moreActions = [
+            [
+                'key' => 'import-excel-default',
+                'name' => 'Import Excel',
+                'html_button' => "<button id='import-excel' type='button' class='btn btn-sm btn-info radius-6' href='#' data-bs-toggle='modal' data-bs-target='#modalImportDefault' title='Import Excel' ><i class='ti ti-upload'></i></button>"
+            ],
+            [
+                'key' => 'export-excel-default',
+                'name' => 'Export Excel',
+                'html_button' => "<a id='export-excel' data-base-url='".$baseUrlExcel."' class='btn btn-sm btn-success radius-6' target='_blank' href='" . url($this->generalUri . '-export-excel-default') . "'  title='Export Excel'><i class='ti ti-cloud-download'></i></a>"
+            ],
+            [
+                'key' => 'export-pdf-default',
+                'name' => 'Export Pdf',
+                'html_button' => "<a id='export-pdf' data-base-url='".$baseUrlPdf."' class='btn btn-sm btn-danger radius-6' target='_blank' href='" . url($this->generalUri . '-export-pdf-default') . "' title='Export PDF'><i class='ti ti-file'></i></a>"
+            ],
+        ];
+
+        $permissions =  $this->arrPermissions;
+        if ($this->dynamicPermission) {
+            $permissions = (new Constant())->permissionByMenu($this->generalUri);
+        }
+        $layout = (request('from_ajax') && request('from_ajax') == true) ? 'easyadmin::backend.idev.list_drawer_ajax' : 'easyadmin::backend.idev.list_drawer';
+        if(isset($this->drawerLayout)){
+            $layout = $this->drawerLayout;
+        }
+        $data['permissions'] = $permissions;
+        $data['more_actions'] = $moreActions;
+        $data['headerLayout'] = $this->pageHeaderLayout;
+        $data['table_headers'] = $this->tableHeaders;
+        $data['title'] = $this->title;
+        $data['uri_key'] = $this->generalUri;
+        $data['uri_list_api'] = route($this->generalUri . '.listapi');
+        $data['uri_create'] = route($this->generalUri . '.create');
+        $data['url_store'] = route($this->generalUri . '.store');
+        $data['fields'] = $this->fields();
+        $data['edit_fields'] = $this->fields('edit');
+        $data['actionButtonViews'] = $this->actionButtonViews;
+        $data['templateImportExcel'] = "#";
+        $data['import_scripts'] = $this->importScripts;
+        $data['import_styles'] = $this->importStyles;
+        $data['filters'] = $this->filters();
+        
+        return view($layout, $data);
+    }
+
+    protected function defaultDataQuery()
+    {
+        $filters = [];
+        $orThose = null;
+        $orderBy = 'id';
+        $orderState = 'DESC';
+        if (request('search')) {
+            $orThose = request('search');
+        }
+        if (request('order')) {
+            $orderBy = request('order');
+            $orderState = request('order_state');
+        }
+
+        $dataQueries = Employee::join('departments', 'departments.id', '=', 'employees.department_id')
+            ->join('positions', 'positions.id', '=', 'employees.position_id')
+            ->join('qualifications', 'qualifications.id', '=', 'employees.qualification_id')
+            ->where($filters)
+            ->where(function ($query) use ($orThose) {
+                $query->where('employees.first_name', 'LIKE', '%' . $orThose . '%')
+                    ->orWhere('employees.email', 'LIKE', '%' . $orThose . '%')
+                    ->orWhere('departments.name', 'LIKE', '%' . $orThose . '%')
+                    ->orWhere('positions.name', 'LIKE', '%' . $orThose . '%')
+                    ->orWhere('qualifications.name', 'LIKE', '%' . $orThose . '%');
+            })
+                ->orderBy($orderBy, $orderState)
+                ->select(
+                'employees.*',
+                'departments.name as department',
+                'positions.name as position',
+                'qualifications.name as qualification'
+            );
+
+        return $dataQueries;
+    }
 
     protected function fields($mode = "create", $id = '-')
     {
@@ -135,7 +222,6 @@ class EmployeeController extends DefaultController
         
         return $fields;
     }
-
 
     protected function rules($id = null)
     {
--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Department;
 use App\Models\Position;
 use Idev\EasyAdmin\app\Http\Controllers\DefaultController;
+use Idev\EasyAdmin\app\Helpers\Constant;
 
 class PositionController extends DefaultController
 {
@@ -12,6 +13,7 @@ class PositionController extends DefaultController
     protected $title;
     protected $generalUri;
     protected $tableHeaders;
+    private $tableHeadersWithCb;
     // protected $actionButtons;
     // protected $arrPermissions;
     protected $importExcelConfig;
@@ -26,9 +28,19 @@ class PositionController extends DefaultController
         $this->tableHeaders = [
                     ['name' => 'No', 'column' => '#', 'order' => true], 
                     ['name' => 'Name', 'column' => 'name', 'order' => true], 
-                    ['name' => 'Department', 'column' => 'department_id', 'order' => true], 
+                    ['name' => 'Department', 'column' => 'department', 'order' => true], 
                     ['name' => 'Created at', 'column' => 'created_at', 'order' => true],
                     ['name' => 'Updated at', 'column' => 'updated_at', 'order' => true],
+        ];
+
+        $this->tableHeadersWithCb = [
+            ['name' => 'Content', 'column' => 'content', 'order' => true],
+            ['name' => 'Type', 'column' => 'type', 'order' => true],
+            ['name' => 'Aspect', 'column' => 'aspect', 'order' => true],
+            ['name' => 'Event', 'column' => 'event_name', 'order' => true],
+            ['name' => 'Department', 'column' => 'department', 'order' => true],
+            ['name' => 'Created at', 'column' => 'created_at', 'order' => true],
+            ['name' => 'Updated at', 'column' => 'updated_at', 'order' => true],
         ];
 
 
@@ -39,6 +51,82 @@ class PositionController extends DefaultController
         ];
     }
 
+    public function index()
+    {
+        $baseUrlExcel = route($this->generalUri.'.export-excel-default');
+        $baseUrlPdf = route($this->generalUri.'.export-pdf-default');
+
+        $moreActions = [
+            [
+                'key' => 'import-excel-default',
+                'name' => 'Import Excel',
+                'html_button' => "<button id='import-excel' type='button' class='btn btn-sm btn-info radius-6' href='#' data-bs-toggle='modal' data-bs-target='#modalImportDefault' title='Import Excel' ><i class='ti ti-upload'></i></button>"
+            ],
+            [
+                'key' => 'export-excel-default',
+                'name' => 'Export Excel',
+                'html_button' => "<a id='export-excel' data-base-url='".$baseUrlExcel."' class='btn btn-sm btn-success radius-6' target='_blank' href='" . url($this->generalUri . '-export-excel-default') . "'  title='Export Excel'><i class='ti ti-cloud-download'></i></a>"
+            ],
+            [
+                'key' => 'export-pdf-default',
+                'name' => 'Export Pdf',
+                'html_button' => "<a id='export-pdf' data-base-url='".$baseUrlPdf."' class='btn btn-sm btn-danger radius-6' target='_blank' href='" . url($this->generalUri . '-export-pdf-default') . "' title='Export PDF'><i class='ti ti-file'></i></a>"
+            ],
+        ];
+
+        $permissions =  $this->arrPermissions;
+        if ($this->dynamicPermission) {
+            $permissions = (new Constant())->permissionByMenu($this->generalUri);
+        }
+        $layout = (request('from_ajax') && request('from_ajax') == true) ? 'easyadmin::backend.idev.list_drawer_ajax' : 'easyadmin::backend.idev.list_drawer';
+        if(isset($this->drawerLayout)){
+            $layout = $this->drawerLayout;
+        }
+        $data['permissions'] = $permissions;
+        $data['more_actions'] = $moreActions;
+        $data['headerLayout'] = $this->pageHeaderLayout;
+        $data['table_headers'] = $this->tableHeaders;
+        $data['title'] = $this->title;
+        $data['uri_key'] = $this->generalUri;
+        $data['uri_list_api'] = route($this->generalUri . '.listapi');
+        $data['uri_create'] = route($this->generalUri . '.create');
+        $data['url_store'] = route($this->generalUri . '.store');
+        $data['fields'] = $this->fields();
+        $data['edit_fields'] = $this->fields('edit');
+        $data['actionButtonViews'] = $this->actionButtonViews;
+        $data['templateImportExcel'] = "#";
+        $data['import_scripts'] = $this->importScripts;
+        $data['import_styles'] = $this->importStyles;
+        $data['filters'] = $this->filters();
+        
+        return view($layout, $data);
+    }
+
+    protected function defaultDataQuery()
+    {
+        $filters = [];
+        $orThose = null;
+        $orderBy = 'id';
+        $orderState = 'DESC';
+        if (request('search')) {
+            $orThose = request('search');
+        }
+        if (request('order')) {
+            $orderBy = request('order');
+            $orderState = request('order_state');
+        }
+
+        $dataQueries = Position::join('departments', 'departments.id', '=', 'positions.department_id')
+        ->where($filters)
+        ->where(function ($query) use ($orThose) {
+            $query->where('positions.name', 'LIKE', '%' . $orThose . '%')
+                ->orWhere('departments.name', 'LIKE', '%' . $orThose . '%');
+        })
+        ->orderBy($orderBy, $orderState)
+        ->select('positions.*', 'departments.name as department');
+
+        return $dataQueries;
+    }
 
     protected function fields($mode = "create", $id = '-')
     {
